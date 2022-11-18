e36d989e0d30fd1204d6e4c8fe68fbe9b21a14b1
root <1415182877@qq.com>
2022-11-18 07:57:19 +0000

    The simplest httpProxy

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # httpProxy
 A simple httpProxy by golang
+
+Listen on port:8888
+Backend address: 127.0.0.1:80
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+)
+
+func startServer() {
+	url, _ := url.Parse("http://127.0.0.1")
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	err := http.ListenAndServe(":8888", proxy)
+	if err != nil {
+		log.Fatalln("ListenAndServe: ", err)
+	}
+}
+
+func main() {
+	startServer()
+}
